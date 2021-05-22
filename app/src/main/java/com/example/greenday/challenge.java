c7ac8b39373790c1aa053156ea73578eb13340e7
sxxxjin <axcv__re@naver.com>
2021-05-22 19:27:22 +0900

    챌린지에 고정된 하단바 생성했음

--- app/src/main/java/com/example/greenday/challenge.java
+++ app/src/main/java/com/example/greenday/challenge.java
@@ -12,19 +12,7 @@ public class challenge extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.challenge_activity);
 
-        //하단부터 getWindow까지는 에뮬에 하단바를 없애는 코드
-        int uiOptions = getWindow().getDecorView().getSystemUiVisibility();
-        int newUiOptions = uiOptions;
-        boolean isImmersiveModeEnabled = ((uiOptions | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) == uiOptions);
-        if (isImmersiveModeEnabled) {
-            Log.i("Is on?", "Turning immersive mode mode off. ");
-        } else {
-            Log.i("Is on?", "Turning immersive mode mode on.");
-        }
-        // 몰입 모드를 꼭 적용해야 한다면 아래의 3가지 속성을 모두 적용시켜야 합니다
-        newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
-        newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
+
+
     }
 }

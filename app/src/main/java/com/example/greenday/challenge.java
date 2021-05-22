6b66d8809f7018ecc42c5e55c3b919db4f33b2cd
wonyoungjun <qpdlsdudwns@naver.com>
2021-05-23 00:31:43 +0900

    한번 바꿔봄

--- app/src/main/java/com/example/greenday/challenge.java
+++ app/src/main/java/com/example/greenday/challenge.java
@@ -1,18 +1,68 @@
 package com.example.greenday;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class challenge extends AppCompatActivity {
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.challenge_activity);
 
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
 
+        ArrayList<Item> list = new ArrayList<>();
 
+        Challenge_DB challenge_db = new Challenge_DB(this);
+        SQLiteDatabase db = challenge_db.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select cha_type, cha_name, cha_intro from challenge", null);
+
+        while(cursor.moveToNext()){
+            Item item = new Item();
+            item.type = cursor.getString(0);
+            item.name = cursor.getString(1);
+            item.intro = cursor.getString(2);
+            list.add(item);
+        }
+        db.close();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new MyAdapter(list));
+        recyclerView.addItemDecoration(new MyItemDecoration(list));
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<MyViewHolder>{
+        ArrayList<Item> list;
+
+        public MyAdapter(ArrayList<Item> list){this.list = list;}
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.)
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }

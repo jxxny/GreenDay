90ba8149596f4747494df8dcc2815642f9f20cb2
wonyoungjun <qpdlsdudwns@naver.com>
2021-05-29 01:58:35 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/challenge_list.xml

--- app/src/main/java/com/example/greenday/Main2Activity.java
+++ app/src/main/java/com/example/greenday/Main2Activity.java
@@ -1,14 +1,63 @@
 package com.example.greenday;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class Main2Activity extends AppCompatActivity {
+    Button rank, trash, home, shop, user;
+    LinearLayout mainontainer;
+    FragmentManager fragmentManager;
+    ChallengeFragment challengeFragment;
+    ShopFragment shopFragment;
+    FragmentTransaction ft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        rank = (Button)findViewById(R.id.rank);
+        shop = (Button)findViewById(R.id.shop);
+        mainontainer = (LinearLayout)findViewById(R.id.main_container);
+
+        fragmentManager = getSupportFragmentManager();
+
+        challengeFragment = new ChallengeFragment();
+        shopFragment = new ShopFragment();
+
+        rank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ft = fragmentManager.beginTransaction();
+                ft.addToBackStack(null);
+                ft.replace(R.id.main_container, challengeFragment);
+                ft.commit();
+            }
+        });
+
+        shop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ft = fragmentManager.beginTransaction();
+                ft.addToBackStack(null);
+                ft.replace(R.id.main_container, shopFragment);
+                ft.commit();
+            }
+        });
+
+
     }
+
 }
--- app/src/main/java/com/example/greenday/TestActivity.java
+++ app/src/main/java/com/example/greenday/TestActivity.java
@@ -19,9 +19,7 @@ import java.util.ArrayList;
 public class TestActivity extends AppCompatActivity {
 
     LinearLayout main_container;
-    ImageView button1, button2, button3, button4, button5;
-    ChallengeFragment challengeFragment;
-    RankFragment rankFragment;
+    Button button1, button2, button3, button4, button5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,36 +27,12 @@ public class TestActivity extends AppCompatActivity {
         setTitle("뷰페이저테스트");
 
         main_container = (LinearLayout)findViewById(R.id.main_container);
-        ViewPager pager = (ViewPager)findViewById(R.id.view_pager);
-        button1 = (ImageView)findViewById(R.id.rank2);
-        button2 = (ImageView)findViewById(R.id.trash2);
-        button3 = (ImageView)findViewById(R.id.home2);
-        button4 = (ImageView)findViewById(R.id.shop2);
-        button5 = (ImageView)findViewById(R.id.user2);
-
-        MyPagerAdapter pagerAdapter =new MyPagerAdapter(getSupportFragmentManager());
-        pager.setAdapter(pagerAdapter);
+        button1 = (Button)findViewById(R.id.rank);
+        button2 = (Button)findViewById(R.id.trash);
+        button3 = (Button)findViewById(R.id.home);
+        button4 = (Button)findViewById(R.id.shop);
+        button5 = (Button)findViewById(R.id.user);
 
     }
 
-    class MyPagerAdapter extends FragmentPagerAdapter{
-        ArrayList<Fragment> fragments;
-
-        public MyPagerAdapter(@NonNull FragmentManager fm) {
-            super(fm);
-            fragments = new ArrayList<>();
-            fragments.add(new ChallengeFragment());
-            fragments.add(new RankFragment());
-        }
-        @Override
-        public int getCount() {
-            return 2;
-        }
-
-        @NonNull
-        @Override
-        public Fragment getItem(int position) {
-            return fragments.get(position);
-        }
-    }
 }

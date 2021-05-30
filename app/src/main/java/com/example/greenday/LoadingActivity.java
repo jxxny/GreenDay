b0448ea9ac432bf730942019ea0b78fc1c43a9f3
wonyoungjun <qpdlsdudwns@naver.com>
2021-05-31 00:49:33 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/challenge_list.xml

---
+++ app/src/main/java/com/example/greenday/LoadingActivity.java
@@ -0,0 +1,67 @@
+package com.example.greenday;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.ClipDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.nio.Buffer;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class LoadingActivity extends AppCompatActivity {
+    private ClipDrawable mImageDrawable;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        ImageView img = (ImageView)findViewById(R.id.imageView1);
+        mImageDrawable = (ClipDrawable) img.getDrawable();
+        textView = (TextView)findViewById(R.id.textView);
+
+        int i = 0;
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    final int[] per = {0};
+                    while(per[0] != 100){
+                        try{
+                            Thread.sleep(600);
+                        }catch(Exception e){
+                            e.printStackTrace();
+                        }
+                        final int percent = (int) Math.floor(Math.random() * 7);
+
+                        textView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                per[0] += percent;
+                                if(per[0] > 100)
+                                    per[0] = 100;
+                                mImageDrawable.setLevel(mImageDrawable.getLevel() + 100 * percent);
+                                textView.setText(per[0] + "%");
+
+                            }
+                        });
+                    }
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            }).start();
+    }
+
+
+}

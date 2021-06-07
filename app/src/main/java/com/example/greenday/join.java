cc9de31382993e1218906a69926e07f4451f00ff
wonyoungjun <qpdlsdudwns@naver.com>
2021-06-07 13:10:06 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/greenday/ChildChallengeFragment.java

--- app/src/main/java/com/example/greenday/join.java
+++ app/src/main/java/com/example/greenday/join.java
@@ -2,16 +2,25 @@ package com.example.greenday;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class join extends AppCompatActivity {
     ImageView back_arrow;
     Button nickname_check_button, id_check_button, join_button;
+    EditText insert_pwd, insert_repwd;
+    ImageView setImage;
+    Pattern pattern;
+    Matcher matcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,13 @@ public class join extends AppCompatActivity {
         join_button = (Button)findViewById(R.id.join_button);
         id_check_button = (Button)findViewById(R.id.id_check_button);
         nickname_check_button = (Button)findViewById(R.id.nickname_check_button);
+        insert_pwd = (EditText)findViewById(R.id.insert_pwd);
+        insert_repwd = (EditText)findViewById(R.id.insert_repwd);
+        setImage = (ImageView)findViewById(R.id.setImage);
+
+        String symbol = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&]).{8,15}.$";
+
+        pattern = Pattern.compile(symbol);
 
         join_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +54,59 @@ public class join extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        insert_pwd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String pwd = insert_pwd.getText().toString();
+                String repwd = insert_repwd.getText().toString();
+                matcher = pattern.matcher(pwd);
+                if(matcher.find() && pwd.equals(repwd)){
+                    setImage.setImageResource(R.drawable.checkicon);
+                } else if(repwd.equals("") && pwd.equals("")){
+                    setImage.setImageResource(R.drawable.setimage);
+                }
+                else{
+                    setImage.setImageResource(R.drawable.xicon);
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        insert_repwd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String pwd = insert_pwd.getText().toString();
+                String repwd = insert_repwd.getText().toString();
+                matcher = pattern.matcher(pwd);
+
+                if(matcher.find() && pwd.equals(repwd)){
+                    setImage.setImageResource(R.drawable.checkicon);
+                }else if(repwd.equals("") && pwd.equals("")){
+                    setImage.setImageResource(R.drawable.setimage);
+                }
+                else{
+                    setImage.setImageResource(R.drawable.xicon);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
     }
 }

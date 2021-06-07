7f20213d048352d02622495b1d923cd106bb68b3
sxxxjin <axcv__re@naver.com>
2021-06-07 23:43:26 +0900

    .

--- app/src/main/java/com/example/greenday/TrashFragment.java
+++ app/src/main/java/com/example/greenday/TrashFragment.java
@@ -1,5 +1,6 @@
 package com.example.greenday;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TrashFragment extends Fragment {
+    Button paper_button;
 
     public TrashFragment() {
         // Required empty public constructor
@@ -23,6 +26,16 @@ public class TrashFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trash, container, false);
+        View view = inflater.inflate(R.layout.fragment_trash, container, false);
+
+        paper_button = (Button)view.findViewById(R.id.paper_button);
+
+        paper_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), join.class);
+                startActivity(intent);
+            }
+        });
     }
 }
